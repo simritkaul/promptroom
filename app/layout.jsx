5b86d119d7a4490ace2b1220d1d8e63c3ae3827e
Simrit Kaul <47447322+simritkaul@users.noreply.github.com>
2023-09-25 12:32:47 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,28 @@
-import '@styles/globals.css';
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptroom",
-  description: "Discover and Share Cool AI Prompts"
-}
+  description: "Discover and Share Cool AI Prompts",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-    <body>
-      <div className='main'>
-        <div className='gradient'/>
-      </div>
-      <main className="app">
-        <Nav />
-        {children}
-      </main>
-    </body>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
