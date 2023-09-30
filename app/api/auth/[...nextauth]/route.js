c1f928fb563df94dad53c411fe68f64147ef3243
Simrit Kaul <47447322+simritkaul@users.noreply.github.com>
2023-09-30 13:09:55 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,14 +3,20 @@ import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [GoogleProvider({
-    clientId: '',
-    clientSecret: '',
+    clientId: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   })],
   async session({session}) {
 
   },
 
   async signIn({profile}) {
-
+    try {
+      
+    } catch (error) {
+      
+    }
   }
 })
+
+export {handler as GET, handler as POST};
